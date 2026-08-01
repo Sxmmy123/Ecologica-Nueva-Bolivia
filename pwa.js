05e1c3e3d785da065072910c3fd5f8fa718f00d9
Sxmmy <smichmev@gmail.com>
2026-08-01 18:47:34 -0400

    pequeñito error alumnos

--- pwa.js
+++ pwa.js
@@ -1,9 +1,9 @@
 ﻿(function () {
   "use strict";
 
-  const APP_VERSION = "v2.25";
+  const APP_VERSION = "v2.26";
   const CACHE_PREFIX = "nueva-bolivia-pwa-";
-  const CURRENT_CACHE = "nueva-bolivia-pwa-v2.25";
+  const CURRENT_CACHE = "nueva-bolivia-pwa-v2.26";
   const CLEANUP_KEY = "__pwaCacheCleanupVersion";
   const isLocalFile = location.protocol === "file:";
 
@@ -26,7 +26,7 @@
     window.addEventListener("load", async () => {
       await cleanOldCachesOnce();
 
-      navigator.serviceWorker.register("./service-worker.js?v=v2.25", { updateViaCache: "none" })
+      navigator.serviceWorker.register("./service-worker.js?v=v2.26", { updateViaCache: "none" })
         .then(registration => {
           if (registration.waiting) registration.waiting.postMessage({ type: "SKIP_WAITING" });
           registration.addEventListener("updatefound", () => {
--- sincronizacion/firebase/firebase-core.js
+++ sincronizacion/firebase/firebase-core.js
@@ -24,6 +24,6 @@
   
 
   window.FirebaseSchoolSync = window.FirebaseSchoolSync || {};
-  window.FirebaseSchoolSync.version = "v2.25";
+  window.FirebaseSchoolSync.version = "v2.26";
   window.FirebaseSchoolSync.databaseRoot = "sistemaEscolar";
 })();
